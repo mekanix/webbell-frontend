e502e405cf661570ec1619dd1fd1f72341cc1163
Goran Mekić <meka@tilda.center>
2020-10-21 19:04:13 +0200

    Position add button

--- src/pages/social/list.js
+++ src/pages/social/list.js
@@ -152,7 +152,7 @@ class SocialsList extends React.Component {
         <Fab
           color="primary"
           onClick={this.openAdd}
-          style={{ position: 'absolute', right: 20 }}
+          style={{ position: 'absolute', top: 90, right: 20 }}
         >
           <AddIcon />
         </Fab>
